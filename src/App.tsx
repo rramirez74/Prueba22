da4145f68917feb6fb66f6b857b7f04d4fc0ea54
Rafa Ramirez <rramirez74@gmail.com>
2022-10-04 10:54:19 +0200

    make it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Today from "./components/Today";
+import Watch from "./components/Watch";
 
 function App() {
+  const isPause = useRef(false);
+  const [image, setImage] = useState("");
+  const url = "https://picsum.photos/200";
+
+  const handleClick = () => {
+    isPause.current = !isPause.current;
+  };
+
+  const fetchImage = async () => {
+    const fetchImage = await fetch(url);
+    const newImage = await fetchImage.url;
+    return newImage;
+  };
+
+  const newImage = () => {
+    if (!isPause.current) {
+      fetchImage().then((tmpImage) => {
+        //console.log(tmpImage);
+        setImage(tmpImage);
+      });
+    }
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      newImage();
+    }, 4000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="neo">
+        <div className="frameContainer">
+          <h1 className="title">IMAGES</h1>
+          <div className="containerToday">
+            <div className="containerWatch">
+              <Watch />
+            </div>
+            <div className="containerData" onClick={handleClick}>
+              <Today img={image} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
